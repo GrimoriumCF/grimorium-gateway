e9f589d58663f46a9008d38f6a181416e4d77ab5
println <murilo_pereira@terceiros.sicredi.com.br>
2024-01-15 20:00:11 -0300

    Adicionando gateway

---
+++ src/test/java/br/com/grimorium/server/GrimoriumServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grimorium.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrimoriumServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
